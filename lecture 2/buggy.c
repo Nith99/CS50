aedbaad7b6913ddb0d7b055f3179b0b727cfd3b8
Nith99 <lanith9000@gmail.com>
2024-08-05 20:07:25 -0300

    L2

---
+++ lecture 2/buggy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_column(int height);
+
+int main(void){
+
+    int n;
+    scanf("%i", &n);
+    print_column(n);
+
+}
+
+void print_column(int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        printf("#\n");
+    }
+}
