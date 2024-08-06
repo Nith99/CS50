48ec7d8d62f3f30c244b0216b09d30a28dc4166c
Nith99 <lanith9000@gmail.com>
2024-08-06 10:44:22 -0300

    lecture 2

--- lecture 2/scores.c
+++ lecture 2/scores.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+float average(int length, int array[]);
+
 int main(void){ 
+    int number_of_scores;
+    printf("Numero de notas: ");
+    scanf("%i", &number_of_scores);
 
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[number_of_scores];
 
-    printf("Average: %f\n", ((score1 + score2 + score3)/3.0));
+    for (int i = 0; i < number_of_scores; i++){
+        printf("Escreva o valor %i: ", i);
+        scanf("%i", &scores[i]);
+    }
 
+    printf("Average: %f\n", average(number_of_scores, scores));
+
+}
+
+float average(int length, int array[]){
+
+    float scores_sum = 0; //Tá em float para o resultado da divisão ser em float.
+    
+    for (int i = 0; i < length; i++){   
+        scores_sum+= array[i];
+    }
+
+    return (scores_sum / length);
 }
